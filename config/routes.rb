d81da66ea65c3cf611ff7fbe0d28fec8af40d664
imaimiami <bkhr.dat@gmail.com>
2014-05-18 13:27:37 +0900

    Like gecko

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Globe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'test' => 'welcome#test'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
